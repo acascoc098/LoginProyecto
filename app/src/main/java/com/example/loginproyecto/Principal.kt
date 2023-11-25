ef1c5484fb3976aa746e75d04f2e7c5ec952dc5f
acascoc098 <acascoc098@g.educaand.es>
2023-11-25 04:36:57 +0100

    Primera versión del cardview.

--- app/src/main/java/com/example/loginproyecto/Principal.kt
+++ app/src/main/java/com/example/loginproyecto/Principal.kt
@@ -1,4 +1,14 @@
 package com.example.loginproyecto
 
-class Principal {
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.principal)
+    }
 }
