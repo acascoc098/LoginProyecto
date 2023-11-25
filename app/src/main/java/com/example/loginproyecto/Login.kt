49bc8bf7e1c2a99f8fc0c3ced3b7378af12aa7d6
acascoc098 <acascoc098@g.educaand.es>
2023-11-25 03:53:28 +0100

    Subida del proyecto.

---
+++ app/src/main/java/com/example/loginproyecto/Login.kt
@@ -0,0 +1,11 @@
+package com.example.loginproyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.log_in)
+    }
+}
